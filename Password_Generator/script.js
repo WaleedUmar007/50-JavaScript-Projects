1de4a8f5090a16137688c9cd2499c2063aa1c6b6
Waleed Umar <51055552+WaleedUmar007@users.noreply.github.com>
2021-04-28 00:55:16 +0500

    javascript functionality added with buttons

--- Password_Generator/script.js
+++ Password_Generator/script.js
@@ -1,3 +1,12 @@
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
+
 const randomFunc={
     lower:getLowerCase,
     upper:getUpperCase,
@@ -5,8 +14,25 @@ const randomFunc={
     symbol:getRandomSymbol
 
 }
+//Creating an event listener of click for generateEl
+
+generateEl.addEventListener('click',()=>{
+    const length = +lengthEl.value //to generate the password of length as specified. Add +sign before length to make it a number
+    const hasLowerCase = lowercaseEl.checked
+    const hasUpperCase = uppercaseEl.checked
+    const hasNumbers = numbersEl.checked
+    const hasSymbols = symbolsEl.checked
+    console.log (hasLowerCase)
+
+    resultEl.innerText= generatePassword (hasSymbols,hasLowerCase,hasUpperCase,hasNumbers,length) //to generate password
 
 
+})
+function generatePassword(lower,upper,numbers,symbol,length){
+    let generatedPassowrd = ''
+    const typesCount = lower + upper + numbers + symbol 
+    console.log(typesCount)
+}
 
 function getLowerCase(){
     return String.fromCharCode(Math.floor(Math.random()*26) + 97) //97 is ascii code for lowercase alphabets
